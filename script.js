e454eb963885590a94394d51ddf2dda83238c520
Xavier Mbajika <xaviermbajika@Xaviers-MacBook-Pro.local>
2026-07-22 16:07:59 -0700

    Home page deployed

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ console.log("connected")
 window.addEventListener("scroll", () => {
     // 1. Use querySelectorAll to easily loop, or index [0] if it is just one item
     const benefitBoxes = document.getElementById("benefits-section");
-
+    const timelineSection = document.getElementById("timeline")
 
     if (window.scrollY > 300) {
         console.log("scroll-activated")
         benefitBoxes.classList.add("scroll-activated")
-
+    if (window.scrollY > 850) {
+        timelineSection.classList.add("scroll-activated")
+    }
    
 }})
